0024b5d8c2928fea6e2ef57b02f64c3cf700a12d
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-19 10:36:14 -0600

    Update settings.py

--- settings.py
+++ settings.py
@@ -25,12 +25,6 @@ IMAGES_DIR = ROOT / 'images'
 DEFAULT_IMAGE = IMAGES_DIR / 'office_4.jpg'
 DEFAULT_DETECT_IMAGE = IMAGES_DIR / 'office_4_detected.jpg'
 
-# Videos config
-VIDEO_DIR = ROOT / 'videos'
-VIDEO_1_PATH = VIDEO_DIR / 'video_1.mp4'
-VIDEOS_DICT = {
-    'video_1': VIDEO_1_PATH
-}
 
 # ML Model config
 MODEL_DIR = ROOT / 'weights'
