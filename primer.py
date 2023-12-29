16d4741da101fac56c67459ce8414b0e87de8355
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-28 22:06:48 -0600

    Update primer.py

--- primer.py
+++ primer.py
@@ -12,7 +12,7 @@ st.title("Bienvenido a la Aplicación de Detección de Plagas")
 st.write("Esta aplicación ayuda en la detección de insectos y ácaros en la agricultura mexicana.")
 
 # Enlace a tu repositorio de GitHub
-github_repo_url = "https://github.com/tu_usuario/tu_repositorio"
+github_repo_url = "https://github.com/Omaralessandro12/DetecPlagas.git"
 
 if st.button("Ir a la Aplicación en GitHub"):
     st.markdown(f'[Abrir la Aplicación en GitHub]({github_repo_url}BreadcrumbsDetecPlagas/APP.py)')
