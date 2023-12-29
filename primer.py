14a774e2a8e21b4ef90e317803a0e19e1bc19601
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2023-12-28 21:11:18 -0600

    Update primer.py

--- primer.py
+++ primer.py
@@ -12,5 +12,4 @@ st.title("Bienvenido a la Aplicación de Detección de Plagas")
 st.write("Esta aplicación ayuda en la detección de insectos y ácaros en la agricultura mexicana.")
 
 if st.button("Ir a la Aplicación"):
-    st.experimental_rerun()
-
+    st.markdown('<a href="APP.py" target="_blank">Abrir la Aplicación</a>', unsafe_allow_html=True)
