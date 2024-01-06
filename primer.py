909acdf9d9ee12f2c9d528c2467fa6836219a651
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-01-06 14:22:43 -0600

    Update primer.py

--- primer.py
+++ primer.py
@@ -11,7 +11,7 @@ st.title("Bienvenido a la Aplicación de Detección de Plagas")
 st.write("Esta aplicación ayuda en la detección de insectos y ácaros en la agricultura mexicana.")
 
 # Enlace a tu aplicación
-app_url = ""https://github.com/Omaralessandro12/DetecPlagas.git/APP.pyI"  # Reemplaza con la URL correcta de tu aplicación
+app_url = ""https://github.com/Omaralessandro12/DetecPlagas.git/APP.py"  # Reemplaza con la URL correcta de tu aplicación
 
 if st.button("Ir a la Aplicación"):
     webbrowser.open(app_url)
