318a5767efbf16ed3c2d3dafb6f4022efe47c629
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-01-09 17:41:35 -0600

    Update APP.py

--- APP.py
+++ APP.py
@@ -19,13 +19,12 @@ st.set_page_config(
 )
 
 st.title("Detección de Plagas en la agricultura Mexicana")
-st.write ("APLICACIÓN PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
-
+st.write("APLICACIÓN PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
 
 # Barra lateral
 st.sidebar.header("Configuración del modelo de aprendizaje automático")
 
-# Opciones de Modelos 
+# Opciones de Modelos
 model_types_available = ['Yolov8', 'Resnet50']  # Agrega más tareas según sea necesario
 model_type = st.sidebar.multiselect("Seleccionar tarea", model_types_available, default=['Yolov8'])
 
@@ -49,28 +48,33 @@ except Exception as ex:
 fuente_img = st.sidebar.file_uploader("Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
 if fuente_img:
-   
+
     col1, col2 = st.columns(2)
 
     with col1:
         try:
             if fuente_img:
                 uploaded_image = PIL.Image.open(fuente_img)
-                st.image(fuente_img, caption="Imagen Cargada",
-                         use_column_width=True)
+                st.image(fuente_img, caption="Imagen Cargada", use_column_width=True)
         except Exception as ex:
             st.error("Se produjo un error al abrir la imagen.")
             st.error(ex)
 
-    with col2:        
-            if st.sidebar.button('Detectar Plaga'):
-                res = model.predict(uploaded_image                            
-                                    )
+    with col2:
+        if st.sidebar.button('Detectar Plaga'):
+            try:
+                res = model.predict(uploaded_image)
                 boxes = res[0].boxes
                 res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Imagen Detectada',
-                         use_column_width=True)
-                
+                st.image(res_plotted, caption='Imagen Detectada', use_column_width=True)
+
+                with st.expander("Detection Results"):
+                    for box in boxes:
+                        st.write(box.data)
+            except Exception as ex:
+                st.error("Se produjo un error al realizar la detección.")
+                st.error(ex)
+
 
                 
 
