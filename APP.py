f093fbfd020dd2517f28ded64c2692f4e64f0e7c
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-05-19 09:52:57 -0600

    Update APP.py
    Signed-off-by: Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>

--- APP.py
+++ APP.py
@@ -10,7 +10,7 @@ import ajustes
 import ayudaR
 import ayuda
 
-# Configuracion del diseño de la página
+# Configuración del diseño de la página
 
 st.set_page_config(
     page_title="Deteccion de Plagas en la agricultura Mexicana",
@@ -19,23 +19,27 @@ st.set_page_config(
 )
 
 st.title("Detección de Plagas en la agricultura Mexicana")
-st.write("APLICACIÓN PARA LA DETECCIÓN DE INSECTOS E ACAROS EN LA AGRICULTURA MEXICANA ")
+st.write("APLICACIÓN PARA LA DETECCIÓN DE INSECTOS Y ÁCAROS EN LA AGRICULTURA MEXICANA")
 
 # Barra lateral
 st.sidebar.header("Configuración del modelo de aprendizaje automático")
 
 # Opciones de Modelos 
-model_types_available = ['Resnet50']  # Solo se permite Resnet50
-model_type = st.sidebar.multiselect("Seleccionar tarea", model_types_available, default=['Resnet50'])
+model_types_available = ['Yolov8', 'Resnet50']  # Agrega más tareas según sea necesario
+model_type = st.sidebar.multiselect("Seleccionar tarea", model_types_available, default=['Yolov8'])
 
 if not model_type:
     st.error("Debes seleccionar al menos un modelo.")
     st.stop()
 
+# Seleccionado modelo
 selected_task = model_type[0]
 
-# Seleccionado modelo
-model_path = None  # Asignar None para omitir la carga del modelo
+# Seleccionado modelo, corregir para dos modelos a la vez
+if selected_task == 'Yolov8':
+    model_path = Path(ajustes.DETECCIÓN_MODEL)
+elif selected_task == 'Resnet50':
+    model_path = None  # Asignar None para omitir la carga del modelo
 
 # Cargar modelo ML previamente entrenado
 model = None  # Inicializar el modelo como None
@@ -64,7 +68,15 @@ if fuente_img:
 
         with col2:        
             if model is not None:  # Solo ejecutar si se ha cargado un modelo
-                if selected_task == 'Resnet50':
+                if selected_task == 'Yolov8':
+                    res = model.predict(uploaded_image)
+                    boxes = res[0].boxes
+                    num_detections = len(boxes)
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption='Imagen Detectada', use_column_width=True)
+                    # Mostrar el número de detecciones
+                    st.write(f'Número de detecciones: {num_detections}')
+                elif selected_task == 'Resnet50':
                     # llamada a resnet50
                     res = model.predict(uploaded_image)
                     # visualizacion resnet 
