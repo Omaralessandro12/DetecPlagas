4f3c9263dd10d892c1a1b5f49c2d7a55e23ff0d5
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-05-21 16:43:56 -0600

    Update APP.py
    Signed-off-by: Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>

--- APP.py
+++ APP.py
@@ -60,15 +60,17 @@ elif selected_task == 'Resnet50':
     model_path = None  # Asignar None para omitir la carga del modelo
 
 # Cargar modelo ML previamente entrenado
-model = None  # Inicializar el modelo como None
+model_detection = None  # Inicializar el modelo de detección como None
+model_classification = None  # Inicializar el modelo de clasificación como None
+
 if model_path is not None:
     try:
-        if selected_task == 'Yolov8':
-            # Cargar el modelo YOLOv8
-            model = ayuda.load_model(model_path)
-        elif selected_task == 'Resnet50':
-            # Cargar el modelo ResNet50
-            model = load_model(model_path)
+        if 'Yolov8' in model_type:
+            # Cargar el modelo YOLOv8 solo si está seleccionado
+            model_detection = ayuda.load_model(model_path)
+        if 'Resnet50' in model_type:
+            # Cargar el modelo ResNet50 solo si está seleccionado
+            model_classification = load_model(model_path)
     except Exception as ex:
         st.error(f"No se puede cargar el modelo. Verifique la ruta especificada: {model_path}")
         st.error(ex)
@@ -92,28 +94,25 @@ if fuente_img:
                 st.error(ex)
 
         with col2:        
-            if model is not None:  # Solo ejecutar si se ha cargado un modelo
-                if selected_task == 'Yolov8':
-                    res = model.predict(uploaded_image)
-                    boxes = res[0].boxes
-                    num_detections = len(boxes)
-                    res_plotted = res[0].plot()[:, :, ::-1]
-                    st.image(res_plotted, caption='Imagen Detectada', use_column_width=True)
-                    # Mostrar el número de detecciones
-                    st.write(f'Número de detecciones: {num_detections}')
-                    
-                    # Clasificar la imagen detectada con Resnet50
-                    if num_detections > 0:
-                        # Cargar modelo ResNet50
-                        model_resnet = load_model('modelo_resnet50.h5')
-                        class_idx, confidence = model_prediction(np.array(uploaded_image), model_resnet)
-                        st.success(f'LA CLASE ES: {names[class_idx]} con una confianza del {confidence:.2%}')
-                        
-                elif selected_task == 'Resnet50':
-                    # llamada a resnet50
-                    res = model.predict(uploaded_image)
-                    # visualizacion resnet 
-                    st.image(res, caption='Imagen Detectada por Resnet50', use_column_width=True)
-                    # Mostrar el número de detecciones
-                    num_detections = len(res)  # Calculamos el número de detecciones aquí
-                    st.write(f'Número de detecciones: {num_detections}')
+            if model_detection is not None and 'Yolov8' in model_type:
+                # Lógica para detectar utilizando YOLOv8
+                res = model_detection.predict(uploaded_image)
+                boxes = res[0].boxes
+                num_detections = len(boxes)
+                res_plotted = res[0].plot()[:, :, ::-1]
+                st.image(res_plotted, caption='Imagen Detectada', use_column_width=True)
+                # Mostrar el número de detecciones
+                st.write(f'Número de detecciones: {num_detections}')
+
+                # Clasificar la imagen detectada con Resnet50
+                if num_detections > 0 and model_classification is not None and 'Resnet50' in model_type:
+                    class_idx, confidence = model_prediction(np.array(uploaded_image), model_classification)
+                    st.success(f'LA CLASE ES: {names[class_idx]} con una confianza del {confidence:.2%}')
+
+            elif model_classification is not None and 'Resnet50' in model_type:
+                # Lógica para clasificar solo con ResNet50
+                res = model_classification.predict(uploaded_image)
+                st.image(res, caption='Imagen Detectada por Resnet50', use_column_width=True)
+                # Mostrar el número de detecciones
+                num_detections = len(res)  # Calculamos el número de detecciones aquí
+                st.write(f'Número de detecciones: {num_detections}')
