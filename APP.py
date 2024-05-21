f12d490aa634048125a21d3db58fcc40f853c57e
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-05-21 15:20:20 -0600

    Update APP.py
    Signed-off-by: Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>

--- APP.py
+++ APP.py
@@ -1,43 +1,102 @@
 from pathlib import Path
+import PIL
 import numpy as np
-import streamlit as st
 from PIL import Image
 from skimage.transform import resize
+
+# Paquetes externos
+import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.imagenet_utils import preprocess_input
-import ajustes  # Importa ajustes para obtener la ruta del modelo YOLOv8
 
-# Función para cargar el modelo YOLOv8
-def load_yolo_model(model_path):
-    # Aquí deberías implementar tu lógica para cargar el modelo YOLOv8
-    pass
+# Módulos locales
+import ajustes
+import ayudaR
+import ayuda
 
-# Función para detectar objetos utilizando YOLOv8
-def detect_objects_yolo(model, image):
-    # Aquí deberías implementar tu lógica para detectar objetos utilizando YOLOv8
-    pass
+# Configuración del diseño de la página
+st.set_page_config(
+    page_title="Deteccion de Plagas en la agricultura Mexicana",
+    layout="wide",
+    initial_sidebar_state="expanded"
+)
 
-def main():
-    # Cargar modelo YOLOv8
-    yolo_model_path = Path(ajustes.DETECCIÓN_MODEL)
-    yolo_model = load_yolo_model(yolo_model_path)
-    
-    st.title("Clasificación de objetos con YOLOv8 y ResNet50")
-    
-    # Carga de imagen
-    img_file_buffer = st.file_uploader("Cargar imagen", type=["png", "jpg", "jpeg"])
-    
-    if img_file_buffer is not None:
-        # Mostrar imagen original
-        image = Image.open(img_file_buffer)
-        st.image(image, caption="Imagen cargada", use_column_width=True)
-        
-        # Detección de objetos con YOLOv8
-        objects_detected = detect_objects_yolo(yolo_model, image)
-        
-        # Mostrar imagen con recuadros delimitadores de objetos detectados por YOLOv8
-        for obj in objects_detected:
-            st.image(obj['image_with_boxes'], caption=f"Objeto: {obj['class']}", use_column_width=True)
+st.title("Detección de Plagas en la agricultura Mexicana")
+st.write("APLICACIÓN PARA LA DETECCIÓN DE INSECTOS Y ÁCAROS EN LA AGRICULTURA MEXICANA")
 
-if __name__ == '__main__':
-    main()
+# Barra lateral
+st.sidebar.header("Configuración del modelo de aprendizaje automático")
+
+# Opciones de Modelos 
+model_types_available = ['Yolov8', 'Resnet50']  # Agrega más tareas según sea necesario
+model_type = st.sidebar.multiselect("Seleccionar tarea", model_types_available, default=['Yolov8'])
+
+if not model_type:
+    st.error("Debes seleccionar al menos un modelo.")
+    st.stop()
+
+# Seleccionado modelo
+selected_task = model_type[0]
+
+# Seleccionado modelo, corregir para dos modelos a la vez
+if selected_task == 'Yolov8':
+    model_path = Path(ajustes.DETECCIÓN_MODEL)
+elif selected_task == 'Resnet50':
+    model_path = None  # Asignar None para omitir la carga del modelo
+
+# Cargar modelo ML previamente entrenado
+model = None  # Inicializar el modelo como None
+if model_path is not None:
+    try:
+        if selected_task == 'Yolov8':
+            # Cargar el modelo YOLOv8
+            model = ayuda.load_model(model_path)
+        elif selected_task == 'Resnet50':
+            # Cargar el modelo ResNet50
+            model = load_model(model_path)
+    except Exception as ex:
+        st.error(f"No se puede cargar el modelo. Verifique la ruta especificada: {model_path}")
+        st.error(ex)
+
+# Cargar imagen directamente  
+fuente_img = st.sidebar.file_uploader("Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+
+if fuente_img:
+    if st.sidebar.button('Detectar Plaga'):
+        col1, col2 = st.columns(2)
+
+        with col1:
+            try:
+                if fuente_img:
+                    uploaded_image = PIL.Image.open(fuente_img)
+                    st.image(uploaded_image, caption="Imagen Original", use_column_width=True)
+            except Exception as ex:
+                st.error("Se produjo un error al abrir la imagen.")
+                st.error(ex)
+
+        with col2:        
+            if model is not None:  # Solo ejecutar si se ha cargado un modelo
+                if selected_task == 'Yolov8':
+                    res = model.predict(uploaded_image)
+                    boxes = res[0].boxes
+                    num_detections = len(boxes)
+                    res_plotted = res[0].plot()[:, :, ::-1]
+                    st.image(res_plotted, caption='Imagen Detectada', use_column_width=True)
+                    # Mostrar el número de detecciones
+                    st.write(f'Número de detecciones: {num_detections}')
+                    
+                    # Clasificar la imagen detectada con Resnet50
+                    if num_detections > 0:
+                        # Cargar modelo ResNet50
+                        model_resnet = load_model('modelo_resnet50.h5')
+                        class_idx, confidence = model_prediction(np.array(uploaded_image), model_resnet)
+                        st.success(f'LA CLASE ES: {names[class_idx]} con una confianza del {confidence:.2%}')
+                        
+                elif selected_task == 'Resnet50':
+                    # llamada a resnet50
+                    res = model.predict(uploaded_image)
+                    # visualizacion resnet 
+                    st.image(res, caption='Imagen Detectada por Resnet50', use_column_width=True)
+                    # Mostrar el número de detecciones
+                    num_detections = len(res)  # Calculamos el número de detecciones aquí
+                    st.write(f'Número de detecciones: {num_detections}')
