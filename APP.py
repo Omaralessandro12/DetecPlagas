8342ee8da0bb16bc3caa14b78a6a7e9fc337509e
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-05-21 16:54:47 -0600

    Update APP.py
    Signed-off-by: Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>

--- APP.py
+++ APP.py
@@ -44,33 +44,30 @@ st.sidebar.header("Configuración del modelo de aprendizaje automático")
 
 # Opciones de Modelos 
 model_types_available = ['Yolov8', 'Resnet50']  # Agrega más tareas según sea necesario
-model_type = st.sidebar.multiselect("Seleccionar tarea", model_types_available, default=['Yolov8'])
+selected_tasks = st.sidebar.multiselect("Seleccionar tarea", model_types_available, default=['Yolov8'])
 
-if not model_type:
+if not selected_tasks:
     st.error("Debes seleccionar al menos un modelo.")
     st.stop()
 
-# Seleccionado modelo
-selected_task = model_type[0]
-
-# Seleccionado modelo, corregir para dos modelos a la vez
-if selected_task == 'Yolov8':
-    model_path = Path(ajustes.DETECCIÓN_MODEL)
-elif selected_task == 'Resnet50':
-    model_path = None  # Asignar None para omitir la carga del modelo
-
-# Cargar modelo ML previamente entrenado
-model = None  # Inicializar el modelo como None
-if model_path is not None:
+# Cargar modelos según la selección
+models = {}
+if 'Yolov8' in selected_tasks:
+    yolov8_model_path = Path(ajustes.DETECCIÓN_MODEL)
     try:
-        if selected_task == 'Yolov8':
-            # Cargar el modelo YOLOv8
-            model = ayuda.load_model(model_path)
-        elif selected_task == 'Resnet50':
-            # Cargar el modelo ResNet50
-            model = load_model(model_path)
+        yolov8_model = ayuda.load_model(yolov8_model_path)
+        models['Yolov8'] = yolov8_model
     except Exception as ex:
-        st.error(f"No se puede cargar el modelo. Verifique la ruta especificada: {model_path}")
+        st.error(f"No se puede cargar el modelo YOLOv8. Verifique la ruta especificada: {yolov8_model_path}")
+        st.error(ex)
+
+if 'Resnet50' in selected_tasks:
+    resnet50_model_path = 'modelo_resnet50.h5'
+    try:
+        resnet50_model = load_model(resnet50_model_path)
+        models['Resnet50'] = resnet50_model
+    except Exception as ex:
+        st.error(f"No se puede cargar el modelo ResNet50. Verifique la ruta especificada: {resnet50_model_path}")
         st.error(ex)
 
 names = ['ARAÑA ROJA', 'MOSCA BLANCA', 'MOSCA FRUTA', 'PICUDO ROJO','PULGON VERDE']
@@ -92,28 +89,19 @@ if fuente_img:
                 st.error(ex)
 
         with col2:        
-            if model is not None:  # Solo ejecutar si se ha cargado un modelo
-                if selected_task == 'Yolov8':
-                    res = model.predict(uploaded_image)
-                    boxes = res[0].boxes
-                    num_detections = len(boxes)
-                    res_plotted = res[0].plot()[:, :, ::-1]
-                    st.image(res_plotted, caption='Imagen Detectada', use_column_width=True)
-                    # Mostrar el número de detecciones
-                    st.write(f'Número de detecciones: {num_detections}')
+            if 'Yolov8' in models:
+                res = models['Yolov8'].predict(uploaded_image)
+                boxes = res[0].boxes
+                num_detections = len(boxes)
+                res_plotted = res[0].plot()[:, :, ::-1]
+                st.image(res_plotted, caption='Imagen Detectada por YOLOv8', use_column_width=True)
+                st.write(f'Número de detecciones: {num_detections}')
+                
+                if 'Resnet50' in models and num_detections > 0:
+                    class_idx, confidence = model_prediction(np.array(uploaded_image), models['Resnet50'])
+                    st.success(f'LA CLASE ES: {names[class_idx]} con una confianza del {confidence:.2%}')
                     
-                    # Clasificar la imagen detectada con Resnet50
-                    if num_detections > 0:
-                        # Cargar modelo ResNet50
-                        model_resnet = load_model('modelo_resnet50.h5')
-                        class_idx, confidence = model_prediction(np.array(uploaded_image), model_resnet)
-                        st.success(f'LA CLASE ES: {names[class_idx]} con una confianza del {confidence:.2%}')
-                        
-                elif selected_task == 'Resnet50':
-                    # llamada a resnet50
-                    res = model.predict(uploaded_image)
-                    # visualizacion resnet 
-                    st.image(res, caption='Imagen Detectada por Resnet50', use_column_width=True)
-                    # Mostrar el número de detecciones
-                    num_detections = len(res)  # Calculamos el número de detecciones aquí
-                    st.write(f'Número de detecciones: {num_detections}')
+            elif 'Resnet50' in models:
+                class_idx, confidence = model_prediction(np.array(uploaded_image), models['Resnet50'])
+                st.image(uploaded_image, caption='Imagen Detectada por Resnet50', use_column_width=True)
+                st.success(f'LA CLASE ES: {names[class_idx]} con una confianza del {confidence:.2%}')
