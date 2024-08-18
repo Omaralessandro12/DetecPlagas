70313f505fe835f30ed02adb3acbb29f2e2aaf18
Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>
2024-08-17 18:05:23 -0600

    Update APP.py
    Signed-off-by: Omaralessandro12 <146569710+Omaralessandro12@users.noreply.github.com>

--- APP.py
+++ APP.py
@@ -4,12 +4,10 @@ import numpy as np
 from PIL import Image
 from skimage.transform import resize
 
-# Paquetes externos
 import streamlit as st
 from tensorflow.keras.models import load_model
 from tensorflow.keras.applications.imagenet_utils import preprocess_input
 
-# Módulos locales
 import ajustes
 import ayudaR
 import ayuda
@@ -23,27 +21,27 @@ def model_prediction(img, model):
     x = preprocess_input(img_resize * 255)
     x = np.expand_dims(x, axis=0)
     
-    preds = model.predict(x)[0]  # Solo obtenemos las predicciones para la primera imagen (índice 0)
-    class_idx = np.argmax(preds)  # Índice de la clase predicha
-    confidence = preds[class_idx]  # Nivel de confianza de la predicción
+    preds = model.predict(x)[0]
+    class_idx = np.argmax(preds)
+    confidence = preds[class_idx]
     
     return class_idx, confidence
 
 # Configuración del diseño de la página
 st.set_page_config(
-    page_title="Deteccion y clasificacion de Plagas en la Agricultura Mexicana",
+    page_title="Detección y Clasificación de Plagas en la Agricultura Mexicana",
     layout="wide",
     initial_sidebar_state="expanded"
 )
 
 st.title("Detección y Clasificación de Plagas en la Agricultura Mexicana")
-st.write("Aplicación que ayuda a identificar las cinco plagas más comunes en la agricultura mexicana,como la araña roja, el pulgón verde, la mosca blanca, la mosca de la fruta y el picudo rojo.")
+st.write("Aplicación que ayuda a identificar las cinco plagas más comunes en la agricultura mexicana, como la araña roja, el pulgón verde, la mosca blanca, la mosca de la fruta y el picudo rojo.")
 
 # Barra lateral
 st.sidebar.header("Configuración del modelo de aprendizaje automático")
 
 # Opciones de Modelos 
-model_types_available = ['Yolov8', 'Resnet50']  # Agrega más tareas según sea necesario
+model_types_available = ['Yolov8', 'Resnet50']
 selected_tasks = st.sidebar.multiselect("Seleccionar tarea", model_types_available, default=['Yolov8'])
 
 if not selected_tasks:
@@ -72,36 +70,54 @@ if 'Resnet50' in selected_tasks:
 
 names = ['ARAÑA ROJA', 'MOSCA BLANCA', 'MOSCA FRUTA', 'PICUDO ROJO','PULGON VERDE']
 
+# Imágenes predeterminadas
+default_images = {
+    "Araña Roja": "Imagenes/ar.jpg",
+    "Mosca Blanca": "Imagenes/mb.jpg",
+    "Mosca de la Fruta": "Imagenes/mf.jpg",
+    "Picudo Rojo": "Imagenes/pr.jpg",
+    "Pulgón Verde": "Imagenes/pv.jpg"
+}
+
+# Seleccionar imagen predeterminada
+selected_image = st.sidebar.selectbox("Selecciona una imagen predeterminada", list(default_images.keys()))
+
 # Cargar imagen directamente  
-fuente_img = st.sidebar.file_uploader("Elige una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
+fuente_img = st.sidebar.file_uploader("O sube una imagen...", type=("jpg", "jpeg", "png", 'bmp', 'webp'))
 
+# Cargar la imagen seleccionada
 if fuente_img:
-    if st.sidebar.button('Detectar Plaga'):
-        col1, col2 = st.columns(2)
+    img = PIL.Image.open(fuente_img)
+else:
+    img = PIL.Image.open(default_images[selected_image])
 
-        with col1:
-            try:
-                if fuente_img:
-                    uploaded_image = PIL.Image.open(fuente_img)
-                    st.image(uploaded_image, caption="Imagen Original", use_column_width=True)
-            except Exception as ex:
-                st.error("Se produjo un error al abrir la imagen.")
-                st.error(ex)
+# Mostrar imagen seleccionada
+st.image(img, caption=f"Imagen: {selected_image}", use_column_width=True)
 
-        with col2:        
-            if 'Yolov8' in models:
-                res = models['Yolov8'].predict(uploaded_image)
-                boxes = res[0].boxes
-                num_detections = len(boxes)
-                res_plotted = res[0].plot()[:, :, ::-1]
-                st.image(res_plotted, caption='Imagen Detectada por YOLOv8', use_column_width=True)
-                st.write(f'Número de detecciones: {num_detections}')
+if st.sidebar.button('Detectar Plaga'):
+    col1, col2 = st.columns(2)
+
+    with col1:
+        try:
+            st.image(img, caption="Imagen Original", use_column_width=True)
+        except Exception as ex:
+            st.error("Se produjo un error al abrir la imagen.")
+            st.error(ex)
+
+    with col2:        
+        if 'Yolov8' in models:
+            res = models['Yolov8'].predict(img)
+            boxes = res[0].boxes
+            num_detections = len(boxes)
+            res_plotted = res[0].plot()[:, :, ::-1]
+            st.image(res_plotted, caption='Imagen Detectada por YOLOv8', use_column_width=True)
+            st.write(f'Número de detecciones: {num_detections}')
+            
+            if 'Resnet50' in models and num_detections > 0:
+                class_idx, confidence = model_prediction(np.array(img), models['Resnet50'])
+                st.success(f'LA CLASIFICACION ES: {names[class_idx]} con una confianza del {confidence:.2%}')
                 
-                if 'Resnet50' in models and num_detections > 0:
-                    class_idx, confidence = model_prediction(np.array(uploaded_image), models['Resnet50'])
-                    st.success(f'LA CLASIFICACION ES: {names[class_idx]} con una confianza del {confidence:.2%}')
-                    
-            elif 'Resnet50' in models:
-                class_idx, confidence = model_prediction(np.array(uploaded_image), models['Resnet50'])
-                st.image(uploaded_image, caption='Imagen Detectada por Resnet50', use_column_width=True)
-                st.success(f'LA CLASIFICACION ES  {names[class_idx]} con una confianza del {confidence:.2%}')
+        elif 'Resnet50' in models:
+            class_idx, confidence = model_prediction(np.array(img), models['Resnet50'])
+            st.image(img, caption='Imagen Detectada por Resnet50', use_column_width=True)
+            st.success(f'LA CLASIFICACION ES  {names[class_idx]} con una confianza del {confidence:.2%}')
